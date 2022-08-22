eee6003775f532e5166f2474c5d2150465eb65e3
arifhossain <manabethreeplusone@gmail.com>
2022-08-23 02:27:50 +0600

    first commit

--- js/player.js
+++ js/player.js
@@ -1,1 +1,51 @@
-console.log('This is javascript file');
+const playerNameList = [];
+
+//  showPlayer Function
+function showPlayer() {
+    const showPlayName = document.getElementById('selection');
+    showPlayName.innerText = '';
+    for (let i = 0; i < playerNameList.length; i++) {
+        const tr = document.createElement("tr");
+        tr.innerHTML = `<th>${i + 1}.</th>
+        <th>${playerNameList[i]}</th>`;
+        if (i < 5) {
+            showPlayName.appendChild(tr);
+        }
+        else {
+            alert("Sorry!! You can not add more than 5 players");
+            break;
+        }
+
+    }
+}
+
+
+// player selection
+
+function selectPlayer(selected) {
+    const playerName = selected.parentNode.children[0].innerText;
+    if (playerNameList.length < 5) {
+        playerNameList.push(playerName);
+        selected.disabled = true;
+    }
+    else {
+        alert("sorry!! You can not add more than 5 players");
+    }
+    showPlayer();
+}
+// +++++++++Player Name show Part++++++++++ //
+document.getElementById('player-cost-btn').addEventListener('click', function () {
+    const totalPlayer = playerNameList.length;
+    let everyPlayer = getInputValueById('player');
+    if (playerNameList.length < 6 && (isNaN(everyPlayer) === false)) {
+        const totalExpense = everyPlayer * totalPlayer;
+        document.getElementById('expenses').innerText = totalExpense;
+    }
+    else if (everyPlayer < 1) {
+        alert('please select player before');
+    }
+    else {
+        alert('please select player before');
+    }
+
+})
