307b82a11ff2532b55f95197f06f040fa9f17663
arifhossain <manabethreeplusone@gmail.com>
2022-08-23 20:55:47 +0600

    sixth commit

---
+++ js/utiliteis.js
@@ -0,0 +1,39 @@
+// input-value
+function getIDValue(valueId) {
+    const elemntId = document.getElementById(valueId);
+    const idValueString = elemntId.value;
+    return idValueString;
+}
+
+// innerText
+function getIDInnerText(innerTextId) {
+    const elemntId = document.getElementById(innerTextId);
+    const idTextString = elemntId.innerText;
+    return idTextString;
+}
+
+// makeFloat
+function makeFloat(str) {
+    const float = parseFloat(parseFloat(str).toFixed(2));
+    // console.log(float);
+    return float;
+}
+
+// makeInt
+// function makeInt(str) {
+//     const int = parseInt(str);
+//     return int;
+// }
+
+
+// setTextValue
+function setTextValue(id, newValue) {
+    const textElementId = document.getElementById(id);
+    textElementId.innerText = newValue;
+}
+
+// setInputValue (should be into Empty)
+function setInputValue(id, empty) {
+    const inputId = document.getElementById(id);
+    inputId.value = empty;
+}
